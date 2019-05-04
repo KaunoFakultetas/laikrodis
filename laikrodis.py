e39ebebfc7d244778fda057d930005460f6cde39
Tomas Vanagas <tomasvanagas@ymail.com>
2019-05-04 14:23:43 +0300

    update

--- laikrodis.py
+++ laikrodis.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import datetime as dt
 
-#		   0,  1, 2, 3,  4,  5,  6,  7,  8, 9, 10, 11
+# 0, 1, 2, 3,  4, 5, 6, 7, 8, 9, 10, 11
 outputs = [2, 24, 3, 4, 14, 22, 15, 17, 18, 27, 23, 10]
 
 GPIO.setmode(GPIO.BCM)
